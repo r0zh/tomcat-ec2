448b4bff727b5968a69a9b24fb32f4d984d01d4a
r0zh <r0zh017@proton.me>
2024-01-10 18:05:32 +0100

    create a systemd service

--- script.sh
+++ script.sh
@@ -22,7 +22,7 @@ wget https://dlcdn.apache.org/tomcat/tomcat-10/v10.1.18/bin/apache-tomcat-10.1.1
 
 # Check if the file is downloaded successfully
 if [ ! -f tomcat.tar.gz ]; then
-    echo "Failed to download tomcat. Exiting." 
+    echo "Failed to download tomcat. Exiting."
     exit 1
 fi
 
@@ -48,3 +48,60 @@ sed -i '/<Valve className="org.apache.catalina.valves.RemoteAddrValve"/c\<!-- <V
 sed -i '/allow="127\\.\\d+\\.\\d+\\.\\d+|::1|0:0:0:0:0:0:0:1" \/>/c\allow="127\\.\\d+\\.\\d+\\.\\d+|::1|0:0:0:0:0:0:0:1" \/> -->' /opt/tomcat/webapps/manager/META-INF/context.xml
 sed -i '/<Valve className="org.apache.catalina.valves.RemoteAddrValve"/c\<!-- <Valve className="org.apache.catalina.valves.RemoteAddrValve"' /opt/tomcat/webapps/host-manager/META-INF/context.xml
 sed -i '/allow="127\\.\\d+\\.\\d+\\.\\d+|::1|0:0:0:0:0:0:0:1" \/>/c\allow="127\\.\\d+\\.\\d+\\.\\d+|::1|0:0:0:0:0:0:0:1" \/> -->' /opt/tomcat/webapps/host-manager/META-INF/context.xml
+
+# Store the JAVA_HOME path in a variable
+java_home=$(update-java-alternatives -l | tr -s ' ' | cut -d' ' -f3)
+echo $java_home
+
+# Create a systemd service file for Tomcat
+touch /etc/systemd/system/tomcat.service
+
+# Define the contents of the service file
+content=$(
+    cat <<EOF
+[Unit]
+Description=Tomcat
+After=network.target
+
+[Service]
+Type=forking
+
+User=tomcat
+Group=tomcat
+
+Environment="JAVA_HOME=$java_home"
+Environment="JAVA_OPTS=-Djava.security.egd=file:///dev/urandom"
+Environment="CATALINA_BASE=/opt/tomcat"
+Environment="CATALINA_HOME=/opt/tomcat"
+Environment="CATALINA_PID=/opt/tomcat/temp/tomcat.pid"
+Environment="CATALINA_OPTS=-Xms512M -Xmx1024M -server -XX:+UseParallelGC"
+
+ExecStart=/opt/tomcat/bin/startup.sh
+ExecStop=/opt/tomcat/bin/shutdown.sh
+
+RestartSec=10
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOF
+)
+
+# Add the content to the service file
+echo "$content" >/etc/systemd/system/tomcat.service
+
+# Reload the systemd daemon
+sudo systemctl daemon-reload
+
+# Start the Tomcat service
+sudo systemctl start tomcat
+
+# Enable the Tomcat service to start on boot
+sudo systemctl enable tomcat
+
+# Check if the Tomcat service is active
+if systemctl is-active --quiet tomcat; then # if the service is active (returns 0)
+    echo -e "\033[1;32m Server is running! 🚀 \033[0m"
+else # if the service is not active (returns 3)
+    echo -e "\033[1;31m Something went wrong! ❌ \033[0m"
+fi
