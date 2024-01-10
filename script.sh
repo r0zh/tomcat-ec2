6da963950b5b48e1aa1e81afec76c41fdae17244
r0zh <r0zh017@proton.me>
2024-01-10 13:52:02 +0100

    configure admin users

--- script.sh
+++ script.sh
@@ -21,3 +21,16 @@ sudo tar xzvf apache-tomcat-10*.tar.gz -C /opt/tomcat --strip-components=1
 # Grant tomcat ownership over the extracted installation
 sudo chown -R tomcat:tomcat /opt/tomcat/
 sudo chmod -R u+x /opt/tomcat/bin
+
+# Define privileged users in Tomcat’s configuration
+echo '<role rolename="manager-gui" />' | sudo tee -a /opt/tomcat/conf/tomcat-users.xml
+echo '<user username="manager" password="manager_password" roles="manager-gui" />' | sudo tee -a /opt/tomcat/conf/tomcat-users.xml
+echo '<role rolename="admin-gui" />' | sudo tee -a /opt/tomcat/conf/tomcat-users.xml
+echo '<user username="admin" password="admin_password" roles="manager-gui,admin-gui" />' | sudo tee -a /opt/tomcat/conf/tomcat-users.xml
+
+# Remove the restriction for the Manager page
+sed -i '/<Valve className="org.apache.catalina.valves.RemoteAddrValve"/c\<!-- <Valve className="org.apache.catalina.valves.RemoteAddrValve"' /opt/tomcat/webapps/manager/META-INF/context.xml
+sed -i '/allow="127\\.\\d+\\.\\d+\\.\\d+|::1|0:0:0:0:0:0:0:1" \/>/c\allow="127\\.\\d+\\.\\d+\\.\\d+|::1|0:0:0:0:0:0:0:1" \/> -->' /opt/tomcat/webapps/manager/META-INF/context.xml
+# Repeat for Host Manager
+sed -i '/<Valve className="org.apache.catalina.valves.RemoteAddrValve"/c\<!-- <Valve className="org.apache.catalina.valves.RemoteAddrValve"' /opt/tomcat/webapps/host-manager/META-INF/context.xml
+sed -i '/allow="127\\.\\d+\\.\\d+\\.\\d+|::1|0:0:0:0:0:0:0:1" \/>/c\allow="127\\.\\d+\\.\\d+\\.\\d+|::1|0:0:0:0:0:0:0:1" \/> -->' /opt/tomcat/webapps/host-manager/META-INF/context.xml
