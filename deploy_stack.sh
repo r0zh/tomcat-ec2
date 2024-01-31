e815cc3988e220dac5958d149071cb09c9c71940
r0zh <r0zh017@proton.me>
2024-01-31 19:45:54 +0100

    feat: enhance AWS stack deployment with parameters and output

--- deploy_stack.sh
+++ deploy_stack.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 stack_name=$(yq '.stackName' config.yml | tr -d '"')
 aws cloudformation deploy \
-  --template-file ./stack.yml \
-  --stack-name tomcat \
-  --capabilities CAPABILITY_IAM
+	--template-file ./stack.yml \
+	--stack-name tomcat \
+	--capabilities CAPABILITY_IAM
+if [ $? -eq 0 ]; then
+	aws cloudformation list-exports \
+		--query "Exports[?Name=='TomcatURL'].Value"
+fi
