cfa9fef3fc078c6485c06b92e3596c4859e15c33
r0zh <r0zh017@proton.me>
2024-02-01 09:20:29 +0100

    fix(tomcat_install): correct gradlew command and improve file handling in script

--- tomcat_app_install.sh
+++ tomcat_app_install.sh
@@ -3,7 +3,9 @@
 # cd into the script's directory to ensure we are in the correct place
 cd $(dirname "$0")
 
+# build the app
 cd app/
-./gradlew clear war
+./gradlew clean war
 
-mv build/libs/myapp.war /opt/tomcat/webapps/
+# copy the app to the tomcat webapps directory
+mv build/libs/app.war /opt/tomcat/webapps/app.war
